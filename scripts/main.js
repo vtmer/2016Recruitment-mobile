707fcaca47cb26c5fb4ea29ccd4ee2284db52a55
kyjo2014 <467346087@qq.com>
2016-04-06 11:13:51 +0800

    3th

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-"use strict";$(function(){$("#dowebok").fullpage({sectionsColor:["#aad9f9","#ffffff","#fd8383","#6989bd","#e7aa3a","#aad9f9"]})});
+"use strict";$(function(){$("#dowebok").fullpage({sectionsColor:["#87b7de","#ffffff","#fd8383","#6989bd","#e7aa3a","#aad9f9"]})});
