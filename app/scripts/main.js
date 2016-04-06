b4a5225ef465d5b01176224f0c7822a57b1a09be
kyjo2014 <467346087@qq.com>
2016-04-06 11:03:14 +0800

    2th

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,5 @@
 $(function(){
     $('#dowebok').fullpage({
-    	sectionsColor : ['#aad9f9', '#ffffff', '#fd8383', '#6989bd','#e7aa3a','#aad9f9'],
+    	sectionsColor : ['#87b7de', '#ffffff', '#fd8383', '#6989bd','#e7aa3a','#aad9f9'],
     });
 });
